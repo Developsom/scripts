72658368f9b62aab4c2ae443da649094b9444c12
Developsom <100343391+Developsom@users.noreply.github.com>
2023-06-01 18:16:05 +0200

    example script is currently not displaying

---
+++ src/ExampleScript.java
@@ -0,0 +1,10 @@
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+@ScriptManifest(author = "BackToRS", info = "Test", logo = "", name = "Example Script", version = 1)
+public class ExampleScript extends Script {
+    @Override
+    public int onLoop() throws InterruptedException {
+        return 0;
+    }
+}
